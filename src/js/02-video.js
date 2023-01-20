e8ae548c15d8bd3a17d141a2df4d0117de74105a
Aleksander <sandrix@ukr.net>
2023-01-20 17:12:38 +0200

    completed task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const player = new Player('vimeo-player', {
+  id: 19231868,
+  width: 640,
+});
+
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time')).seconds || 0
+);
+
+player.on('timeupdate', throttle((timeupdate) => {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(timeupdate));
+    console.log(localStorage.getItem('videoplayer-current-time'));
+  }, 1000)
+);
